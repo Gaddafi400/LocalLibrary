9f7f1cbd3fba0278056eedd18dd374f991478822
gaddafi <gaddafiadamu400@gmail.com>
2023-04-21 12:22:04 +0100

    Admin panel modification

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+admin.site.register(Language)
+
+
+class BookInline(admin.StackedInline):
+    model = Book
+    extra = 0
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BooksInstanceInline]
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('id', 'book', 'imprint')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
