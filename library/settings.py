a38da796b9a96057add1008d1385a3553c20fe42
gaddafi <gaddafiadamu400@gmail.com>
2023-04-24 22:16:04 +0100

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['web-production-3640.up.railway.app', '127.0.0.1']
+
+
 
 # Application definition
 
